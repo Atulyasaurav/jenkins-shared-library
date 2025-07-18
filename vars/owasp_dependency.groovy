73810df6d7e7b8bd8334966a49f9664afba15057
atulya_saurav_abhisanchit <119062180+Atulyasaurav@users.noreply.github.com>
2025-07-18 22:06:22 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,11 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    def folders = ['auth', 'client', 'common', 'expiration', 'nats-test', 'orders', 'payments', 'tickets']
+    
+    folders.each { folder ->
+        echo "Running OWASP Dependency Check in ${folder}"
+        dir(folder) {
+            dependencyCheck additionalArguments: "--scan .", odcInstallation: 'OWASP'
+        }
+    }
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
